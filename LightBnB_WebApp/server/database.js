fc96ab4344bc3df9036a60af4e372baf103386f9
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-09-16 17:43:21 -0400

    Implement function to add property new property to the properties table

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -89,13 +89,6 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  // return pool.query(`
-  // SELECT * 
-  // FROM properties
-  // LIMIT $1`, [limit])
-  // .then(result => result.rows)
-  // .catch(err => console.log(err));
-
   const queryParams = [];
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
@@ -157,9 +150,16 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryString = `INSERT INTO
+  properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, number_of_bathrooms, number_of_bedrooms, street, city, province, post_code, country)
+  VALUES
+  ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *`;
+
+  const values = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.number_of_bathrooms, property.number_of_bedrooms, property.street, property.city, property.province, property.post_code, property.country];
+
+  return pool
+  .query(queryString, values)
+  .then(result => console.log(result))
+  .catch((err) => err.message);
 }
 exports.addProperty = addProperty;
